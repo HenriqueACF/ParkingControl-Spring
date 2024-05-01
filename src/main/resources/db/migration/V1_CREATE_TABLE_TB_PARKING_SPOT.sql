6fe7272b9728db370edbc36b355755010b2c5b91
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2024-05-01 17:41:32 -0300

    FEAT: criando migration de vagas no HSQL

---
+++ src/main/resources/db/migration/V1_CREATE_TABLE_TB_PARKING_SPOT.sql
@@ -0,0 +1,12 @@
+CREATE TABLE TB_PARKING_SPOT(
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    parkingSpotNUmber VARCHAR(10) NOT NULL,
+    licensePlateCar VARCHAR(7) NOT NULL,
+    brandCar VARCHAR(70) NOT NULL ,
+    modelCar VARCHAR(70) NOT NULL,
+    colorCar VARCHAR(70) NOT NULL,
+    registrationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    responsibleName VARCHAR(130) NOT NULL,
+    apartament VARCHAR(30) NOT NULL,
+    block VARCHAR(30) NOT NULL
+)
